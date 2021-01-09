bee2bd80b7604219aaf19099eb5441f5c8cfcae4
123123123 <123>
2021-01-09 13:52:33 +0300

    123

---
+++ Repository/Game_AD1/gameunit.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# license: GPLv3
+
+
+class Attacker:
+    _health = None
+    _attack = None
+    _experience = None
+
+    def attack(self, target):
+        target._health -= self._attack
+
+    def is_alive(self):
+        return self._health > 0
